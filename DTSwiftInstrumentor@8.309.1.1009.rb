e048d600b5d65f9cbb9e34024b911418d135ccc0
mobile-agent-release <>
2025-02-26 11:30:44 +0100

    8.309.1 release

--- DTSwiftInstrumentor@8.309.1.1009.rb
+++ DTSwiftInstrumentor@8.309.1.1009.rb
@@ -1,4 +1,4 @@
-class Dtswiftinstrumentor < Formula
+class DtswiftinstrumentorAT830911009 < Formula
   homepage "https://www.dynatrace.com/"
   url "https://mobileagent.downloads.dynatrace.com/ios/8.309.1.1009/dynatrace-mobile-agent-ios-8.309.1.1009-swift-instrumentor.zip"
   sha256 "70e6ab24409d9ed6b1c88716a30e7f4abebeb023bd209e98f0a1c39c22360494"
